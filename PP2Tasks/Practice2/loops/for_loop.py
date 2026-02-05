88129b6a2a35aa304640f9fd64338d5d33c494ba
Темур Сапиев <temursapiev@Temurs-MacBook-Air.local>
2026-02-05 15:42:58 +0500

    prac01

---
+++ PP2Tasks/Practice2/loops/for_loop.py
@@ -0,0 +1,16 @@
+#1
+for i in range(1, 6):
+    print(i)
+#2
+fruits = ["apple", "banana", "cherry"]
+for f in fruits:
+    print(f)
+#3
+total = 0
+for i in range(1, 6):
+    total += i
+print(total)
+#4
+word = "hello"
+for ch in word:
+    print(ch)
